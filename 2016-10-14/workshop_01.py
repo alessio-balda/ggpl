13f7af13f04e278e3767dc215b55b4d19847e20f
ab <ale.balda92@gmail.com>
2016-10-26 01:30:33 +0200

    Improved workshop_01

--- 2016-10-14/workshop_01.py
+++ 2016-10-14/workshop_01.py
@@ -2,36 +2,37 @@
 
 from larlib import *
 
-dy=[8,5]
-hz=[11,18,12]
-
-def buildSpaceFrame(px,py,bx,bz):
+def buildSpaceFrame(px,py,bx,bz,dy,hz):
 	"""Creates and shows a space frame of reinforced concrete."""
-	global dy
-	global hz
-	pillars = buildPillars(px,py,bz)
-	beams = buildBeams(px,bx,bz)
+	pillars = buildPillars(px,py,bz,dy,hz)
+	beams = buildBeams(px,bx,bz,dy,hz)
+	pillars=COLOR(RED)(pillars)
+	beams=COLOR(BLUE)(beams)
 	spaceFrame=STRUCT([pillars,beams])
 	VIEW(spaceFrame)
 	return spaceFrame
 
-def buildPillars(px,py,bz):
+
+def buildPillars(px,py,bz,dy,hz):
 	"""Creates pillars of the building's space frame."""
-	x=[px]
+	x = [px]
 	for i in dy:
 		x.append(-i)
 		x.append(px)
-	y=[py]
-	bases=PROD([QUOTE(x),QUOTE(y)])
+	y = [py]
+	bases = PROD([QUOTE(x),QUOTE(y)])
 	l=0
 	for j in hz:
 		pillars[l]=PROD([bases,QUOTE([j])])
 		l=l+1
-	allPillars=STRUCT([pillars[0],T(3)(hz[0]+bz),pillars[1]])
-	allPillars=STRUCT([allPillars,T(3)(hz[0]+hz[1]+bz*2),pillars[2]])
+	z=hz[0]
+	allPillars=STRUCT([pillars[0],T(3)(z+bz),pillars[1]])
+	for i in range(1,len(hz)-1):
+		z=z+hz[i]
+		allPillars=STRUCT([allPillars,T(3)([z+bz*(i+1)]),pillars[i+1]])
 	return allPillars
 
-def buildBeams(px,bx,bz):
+def buildBeams(px,bx,bz,dy,hz):
 	"""Creates beams of the building's space frame."""
 	x=[]
 	for i in dy:
@@ -41,6 +42,10 @@ def buildBeams(px,bx,bz):
 	beams=PROD([bases,QUOTE([bz])])
 	beams=T(1)(-px/2)(beams)
 	beams=T(3)(hz[0])(beams)
-	allBeams=STRUCT([beams,T(3)(hz[1]+bz),beams])
-	allBeams=STRUCT([allBeams,T(3)(hz[1]+hz[2]+bz*2),beams])
+	z=0
+	allBeams=beams
+	for i in range(1,len(hz)):
+		z=z+hz[i]
+		allBeams=STRUCT([allBeams,T(3)(z+bz*i),beams])
 	return allBeams
+		
